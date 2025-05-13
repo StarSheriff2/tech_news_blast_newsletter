0e9a90fb3c9ab663328a2e03241f5bf39f4e632b
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2025-05-13 14:57:45 -0600

    spec: add test for Groq api

--- spec/apis/groq/openai/v1/groq_api_spec.rb
+++ spec/apis/groq/openai/v1/groq_api_spec.rb
@@ -1,18 +1,24 @@
-require_relative "client_spec"
-require_relative "resources/chat_spec"
+require 'rails_helper'
 
-module Groq
-  module Openai
-    module V1
-      class GroqApi
-        def initialize(api_key:)
-          @client = Client.new(api_key: api_key)
-        end
+RSpec.describe Groq::Openai::V1::GroqApi do
+  let(:api_key) { "test_api_key" }
+  let(:groq_api) { described_class.new(api_key: api_key) }
 
-        def chat
-          Resources::Chat.new(@client)
-        end
-      end
+  describe "#initialize" do
+    it "initializes the client with the provided api_key" do
+      client = groq_api.instance_variable_get(:@client)
+      expect(client).to be_a(Groq::Openai::V1::Client)
+      expect(client.api_key).to eq(api_key)
+    end
+  end
+
+  describe "#chat" do
+    it "returns an instance of Resources::Chat with the correct client" do
+      chat_resource = groq_api.chat
+      expect(chat_resource).to be_a(Groq::Openai::V1::Resources::Chat)
+      client = chat_resource.instance_variable_get(:@client)
+      expect(client).to be_a(Groq::Openai::V1::Client)
+      expect(client.api_key).to eq(api_key)
     end
   end
 end
